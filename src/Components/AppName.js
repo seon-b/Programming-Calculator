04d0eb9551fe654712662a3f00976e56be950f0d
seon-b <86736916+seon-b@users.noreply.github.com>
2023-10-20 15:47:55 -0400

    AppName.js converted to a function component

--- src/Components/AppName.js
+++ src/Components/AppName.js
@@ -1,39 +1,19 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './InputForm.css'
 import './AppName.css'
 
 
 
+function AppName (props){
 
-
-class AppName extends Component{
-    static defaultProps = {
-        formName: "",
-
-      
-    }; 
-
-    constructor(props){
-      super(props);
-      this.state = {
-        inputText: this.props.formName,
+  return(
+      <div className="form-group mt-2">
+         <h3 className='appNameStyle'>{props.formName}</h3>
+         
+      </div>
+    
+        )
         
-        
-      };
-    }
-
-
-
-    render(){
-      return(
-        <div className="form-group mt-2">
-           <h3 className='appNameStyle'>{this.state.inputText}</h3>
-           
-        </div>
-
-    )
-    }
-
 
 
 }
